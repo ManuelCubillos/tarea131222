59a03422c10e6cb4bd8f74999836bf6e45927fc3
ManuelCubillos <maccubillos30@gmail.com>
2022-01-13 21:36:59 -0500

    tarea condicionales

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+let nombres, genero
+nombres = document.getElementById('nombre').value
+genero = document.getElementById('genero').value
+
+function validar() {
+	if ( edad. value< 18){
+		alert("No puede ingresar eres menor de edad")
+		return false
+	}else{
+		alert("Puedes ingresar")
+		if (genero = "Hombre"){
+			alert('Hola' + nombres + 'Debe pagar 35.000')
+			}else{
+				alert('Hola' + nombres + 'Debe pagar 20.000')
+			}
+		}
+	}
+
+   
+
---
+++ js/tarea2.js
@@ -0,0 +1,18 @@
+console.log('ciclo dos en dos')
+
+
+for (let i = 0; i < 100; i = i + 2) {
+    console.log(i)
+}
+
+
+console.log('ciclo de tres en tres')
+
+
+let contador = 0;
+
+
+while(contador < 100) {
+    console.log(contador)
+    contador = contador + 3
+}
